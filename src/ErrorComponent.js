d872e8d1a8a8fcb4bf192f65d65e7a0914bb5f4f
Nihabu <henrikbjonerlie@hotmail.com>
2024-11-01 10:18:22 +0100

    Tweaked info, getting ready for deploy

--- src/ErrorComponent.js
+++ src/ErrorComponent.js
@@ -2,13 +2,12 @@ import React from "react";
 import { useRouteError } from "react-router-dom";
 
 function ErrorComponent(props) {
-	const {error, isError, message} = useRouteError();
+	const { error, isError, message } = useRouteError();
 	return (
 		<div className="">
 			<h1>ERROR!</h1>
 			<p>Sorry, the website has shit its pants. 💩</p>
-			<p>{error}</p>
-			<p>{message}</p>
+			<p>{isError ? message : " "}</p>
 		</div>
 	);
 }
--- src/index.js
+++ src/index.js
@@ -24,6 +24,7 @@ const router = createBrowserRouter([
 		element: <Layout />,
 		errorElement: <ErrorComponent />,
 		children: [
+			{ path: "/*", element: <MainBody /> },
 			{
 				path: "/",
 				element: <MainBody />,
