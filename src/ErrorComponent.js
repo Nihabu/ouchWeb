64df14f73643c6833b845a4b897dc73104516726
Nihabu <henrikbjonerlie@hotmail.com>
2024-11-01 09:36:37 +0100

    Upgrading error-handling

--- src/ErrorComponent.js
+++ src/ErrorComponent.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { useRouteError } from "react-router-dom";
 
 function ErrorComponent(props) {
+	const {error, isError, message} = useRouteError();
 	return (
 		<div className="">
 			<h1>ERROR!</h1>
 			<p>Sorry, the website has shit its pants. 💩</p>
+			<p>{error}</p>
+			<p>{message}</p>
 		</div>
 	);
 }
