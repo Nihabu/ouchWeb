6bc024568fce93f4de1bee49df23aaf8c40f7e8f
Nihabu <henrikbjonerlie@hotmail.com>
2025-01-15 20:38:40 +0100

    Updated upcoming events

--- src/upcomingEventsList.js
+++ src/upcomingEventsList.js
@@ -1,12 +1,5 @@
 const eventsList = [
 	{
-		date: "03.01.2025 - 04.01.2025",
-		location: "Oslo, Tøyen Sportsklubb",
-		locationLink: "https://maps.app.goo.gl/PRDm2SzBRuSDiGHA8",
-		title: "Tøyen Night Hat",
-		info: "The fourth annual Tøyen Night Hat Tournament! Starts Saturday January 3rd at 21:00. Registration via the Oslo Ultimate group on Spond.",
-	},
-	{
 		date: "01.03.2025 - 02.03.2025",
 		location: "Tromsø, TBD",
 		locationLink: null,
