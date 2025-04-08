9ad6d7f22af168b1493919bd80283e27d20b9412
Nihabu <henrikbjonerlie@hotmail.com>
2025-04-08 22:05:13 +0200

    Added NM news

--- src/newsList.js
+++ src/newsList.js
@@ -1,8 +1,22 @@
 const newsList = [
 	{
+		header: "The Norwegian National Championshop 2025",
+		text:
+			"An incredible weekend of exciting Ultimate has come to an end, and we have crowned this year’s Norwegian champions. " +
+			"With 13 teams finding their way to Trondheim for the weekend, the scene was set for a proper showdown, and a show we did get. " +
+			"Sharp cuts, pin-pointed throws and sky high pulls made for an entertaining weekend, for players and spectators alike. " +
+			"Congratulations to Framnes who claimed the NM title! A well-earned victory to cap off a strong season for the club. " +
+			"Bærum Ultimate made an impressive debut, securing a fantastic 2nd place, while Furuset Brickers maintained their spot on the podium with a solid 3rd place. " +
+			"The Spirit Award went to TSI from Tromsø. " +
+			"The Oslo Ultimate scene was well represented at the championship, with Ekeberg (4th), Pancake (5th) and OSI (6th) making the trip in addition to Bærum and Brickers. " +
+			"A huge thank you to Trondheim Frisbeeklubb for organizing such a fantastic NM, and to all the players and teams who made this event so good! " +
+			"Now on to outdoor Ultimate!",
+		posted: "08.04.2025",
+	},
+	{
 		header: "69 Discs North 2025",
 		text:
-			"The great arctic clash that is 69 Discs North is over and done with. We saw 12 teams, incl. 2 from outside the country borders, battle it out on the field 1st and 2nd of March 2025." + 
+			"The great arctic clash that is 69 Discs North is over and done with. We saw 12 teams, incl. 2 from outside the country borders, battle it out on the field 1st and 2nd of March 2025." +
 			"As always there were hard fought matches throughout the weekend and in all parts of the bracket, with some extremely close scores in the semi finals on Sunday. " +
 			"On field 1 we saw Framnes barely beating Pancake 10 to 9, and Brickers " +
 			"having to admit defeat 10 - 11 versus TUSK on field 2. The bronze final saw a repeat of the NM final of 2024, Pancake vs. Brickers, with a similar outcome as back then, Brickers taking 3rd place. " +
@@ -13,10 +27,10 @@ const newsList = [
 	{
 		header: "Second half of 24/25",
 		text:
-			"After a, for some, slow winter off-season, it is time to shake off the winter slumber and finish off what so far has been an action packed 2024/2025-season. " + 
-			"We are already deep into February, and the third and final tournament of the series is just around the corner. On the 1st and 2nd of March we will meet in the polar spring and " + 
-			"battle it out during 69 Discs North, arranged by the legends of Tromsø Ultimate Frisbee. " + 
-			"This is not a tournament you want to miss, as the series this year is closer than it has been for a while, with several teams having a chance at the top spot. " + 
+			"After a, for some, slow winter off-season, it is time to shake off the winter slumber and finish off what so far has been an action packed 2024/2025-season. " +
+			"We are already deep into February, and the third and final tournament of the series is just around the corner. On the 1st and 2nd of March we will meet in the polar spring and " +
+			"battle it out during 69 Discs North, arranged by the legends of Tromsø Ultimate Frisbee. " +
+			"This is not a tournament you want to miss, as the series this year is closer than it has been for a while, with several teams having a chance at the top spot. " +
 			"See you there!",
 		posted: "10.02.2025",
 	},
