55e32d2d292a1395715ad74a9a6e3219ce8854e2
Nihabu <henrikbjonerlie@hotmail.com>
2025-05-13 22:01:20 +0200

    More testing, bug not gone

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ const router = createBrowserRouter([
 		element: <Layout />,
 		errorElement: <ErrorComponent />,
 		children: [
-			{ path: "/*", element: <MainBody /> },
+			// { path: "/*", element: <MainBody /> },
 			{
 				path: "/",
 				element: <MainBody />,
