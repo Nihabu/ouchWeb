16eee34e1a75bc8e4d8cea8630bb9ebdde51a3be
Nihabu <henrikbjonerlie@hotmail.com>
2025-09-11 09:27:14 +0200

    Updated upcoming events

--- src/upcomingEventsList.js
+++ src/upcomingEventsList.js
@@ -1,19 +1,37 @@
 const eventsList = [
 	{
-		date: "late June - early August",
-		location: "Norway",
+		date: "18.-19. October 2025",
+		location: "Bergen",
 		locationLink: null,
-		title: "Summer Offseason",
-		info: "As we are entering summer holidays, make sure to put yourself on attending on all practices and events you plan to attend. Some events might get cancelled as people enjoy their vacations",
+		title: "BrainyDisk '25",
+		info: "The first tournament of the Norwegian Indoor Series 25/26. Format is full mixed.",
 		extraLink: null,
 		extra: null
 	},
 	{
-		date: "6.-7. September 2025",
-		location: "Oslo, Ekebergsletta",
-		locationLink: "https://maps.app.goo.gl/JP1LNztk3qmtMkK67",
-		title: "Oslo Kapp 2025",
-		info: "The 4th Annual Oslo Kapp is happening!",
+		date: "13.-14. December 2025",
+		location: "Fjellhamarhallen, Fjellhamar",
+		locationLink: "https://maps.app.goo.gl/1S5bzPPacjNmx8xm9",
+		title: "Decembercup '25",
+		info: "The second tournament of the Norwegian Indoor Series 25/26. Format is Open / Women.",
+		extraLink: null,
+		extra: "Due to limited amount of FMPs, the womens division is TBD."
+	},
+	{
+		date: "28. February - 1. March 2026",
+		location: "Tromsø",
+		locationLink: null,
+		title: "69 Discs North '26",
+		info: "The third and last tournament of the Norwegian Indoor Series 25/26. Format is loose mixed",
+		extraLink: null,
+		extra: null
+	},
+	{
+		date: "28. February - 1. March 2026",
+		location: "Sotra, Bergen",
+		locationLink: null,
+		title: "NIM 2026",
+		info: "The Norwegian Indoor Championships 2026. Format is loose mixed.",
 		extraLink: null,
 		extra: null
 	},
