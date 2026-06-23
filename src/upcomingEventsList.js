75746d5e9d0a734a061e6dd02d7940cfb870fe0b
daniher <d.c.herman02@gmail.com>
2026-06-23 12:50:14 +0200

    Feat: update event list

--- src/upcomingEventsList.js
+++ src/upcomingEventsList.js
@@ -1,19 +1,10 @@
 const eventsList = [
 	{
-		date: "28. - 29. March 2026",
-		location: "Sotra, Bergen",
-		locationLink: null,
-		title: "NIM 2026",
-		info: "The Norwegian Indoor Championships 2026. Format is loose mixed.",
-		extraLink: null,
-		extra: null
-	},
-	{
-		date: "30. - 31. May 2026",
-		location: "Furuset, Oslo",
-		locationLink: "https://maps.app.goo.gl/zF5mPyUMDGwCryjT9",
-		title: "The Fast and the Furuset: Tokea Drift",
-		info: "The 3rd edition of Furuset Brickers' outdoor tournament. Grass, sun, and good vibes guaranteed!",
+		date: "22. - 23. August",
+		location: "Ekeberg, Oslo",
+		locationLink: "https://maps.app.goo.gl/X9JkoMaivSgBRzbWA",
+		title: "Oslo Kapp",
+                info: "The 4th edition of Oslo Kapp. Grass tournament, typically 6v6, loose mixed.",
 		extraLink: null,
 		extra: null
 	},
